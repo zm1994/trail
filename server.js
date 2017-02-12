b68a7b737f2daffd65897edec476286e581e9954
s <yaroslav-google@mail.ru>
2017-02-12 23:32:03 +0200

    'firstkj'

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get("/api/contacts", function(req, res) {
 });
 
 app.get("/api/availdir/:id", function(req, res) {
-  db.collection(AVAILABLE_DIRECTIONS).find({"departure_id": req.params.id}).toArray(function(err, docs) {
+  db.collection(AVAILABLE_DIRECTIONS).find({"departure_id": 0}).toArray(function(err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get contacts.");
     } else {
@@ -69,7 +69,7 @@ app.get("/api/availdir/:id", function(req, res) {
 });
 
 app.get("/api/airport/:code", function(req, res) {
-  db.collection(AIRPORTS).find({"code_airport": 0}).toArray(function(err, docs) {
+  db.collection(AIRPORTS).find({"code_airport": res.params.code}).toArray(function(err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get contacts.");
     } else {
