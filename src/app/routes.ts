9b075f12f2a0eb50a88d3e110bb76c427680eb05
s <yaroslav-google@mail.ru>
2017-02-13 21:29:37 +0200

    'sdf'

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router'
 import { AppComponent } from './app.component'
 
 export const ROUTES: Routes = [
-    { path: "/", component: AppComponent },
     { path: "**", redirectTo: '/' }
 ]
