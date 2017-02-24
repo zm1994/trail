926c74fb3e53aa5a8ca0c7a082ffded6e3845d80
zm1994 <volniy.ptaha@gmail.com>
2017-02-24 14:19:22 +0200

    l

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-import {Md5} from 'ts-md5/dist/md5';
 
 @Component({
     selector: 'home',
@@ -11,18 +10,8 @@ import {Md5} from 'ts-md5/dist/md5';
 export class HomeComponent {
     private numberBackgroundImage: number;
 
-    constructor(private md5: Md5) {
+    constructor() {
       //get random background image
         this.numberBackgroundImage = Math.floor(Math.random() * 3) + 1
     }
-<<<<<<< HEAD
-
-
-    ngOnInit() {
-      console.log(Md5.hashStr('email password'))
-
-    }
-
-=======
->>>>>>> c2093b82f7cdef0fd1906a75266c11bbab65121f
 }
